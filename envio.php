8897c3eef5403738d609c7c28904435a3611350a
ngmachado1 <54647104+ngmachado1@users.noreply.github.com>
2020-09-12 11:13:51 -0300

    Add files via upload

--- envio.php
+++ envio.php
@@ -1,11 +1,16 @@
 <?php 
 $destino = "nataliagisellemachado@gmail.com";
-if(isset($_POST['enviar'])){
-    if(!empty($_POST['nombre']) && !empty($_POST['asunto']) && !empty($_POST['msg']) && !empty($_POST['email'])){
+if(isset($_POST['msg'])){
         $name = $_POST['nombre'];
         $asunto = $_POST['asunto'];
         $remitente = $_POST['email'];
-        $msg = $_POST['mensaje'];
+        $msg = $_POST['msg'];
+        
+        $cuerpo = "Nombre: ".$_POST['nombre'] ." \r\n";
+        $cuerpo .= "Asunto: ".$_POST['asunto'] ." \r\n";
+        $cuerpo .= "Email: ".$_POST['email'] ." \r\n";
+        $cuerpo .= "Mensaje: ".$_POST['msg'] ." \r\n";
+        
         $headers  = "MIME-Version: 1.0\n";
         $headers .= "Content-type: text/plain; charset=utf-8\n";
         $headers .= "X-Priority: 3\n";
@@ -13,18 +18,6 @@ if(isset($_POST['enviar'])){
         $headers .= "X-Mailer: php\n";
         $headers .= "From: \"".$_POST['nombre']." \" <".$remitente.">\n";
         
-        mail($destino, $asunto, $msg, $headers);
-    }
-}
+        mail($destino, $asunto, $cuerpo, $headers);
+    };
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1> Hola Mundo! </h1>
-</body>
-</html>
